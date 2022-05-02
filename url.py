8a0574e8aa0b54e1db899181a085de4b00abcb0d
wpted <silverhandsome@gmail.com>
2022-05-03 04:01:55 +0800

    Add function get_url(id)

--- url.py
+++ url.py
@@ -3,14 +3,15 @@ import sqlite3
 import os  # For path checking
 from typing import Union  # For Type Hints
 
-configparser = ConfigParser()
+config = ConfigParser()
 config.read('config.ini')
 DB_PATH = config["DATABASE"]["DB"]
 
 
 def initialize_database() -> None:
     """
-    Check if database exists. If so, read table, else, create database and table.
+        Check if database exists. If so, read table, else, create database and table.
+        :return None
     """
     if os.path.exists(DB_PATH):
         connection = sqlite3.connect(DB_PATH)
@@ -18,18 +19,34 @@ def initialize_database() -> None:
         query = "SELECT url FROM sqlite_master where type='table' AND name='urls'"
         cursor.execute(query)
 
-        if cursor.fetchone() is None:  # If URLs table doesn't exist, create it
+        if cursor.fetchone() is None:  # If URLs table doesn't exist, creates it
             create_query = "CREATE TABLE urls (id PRIMARY KEY, url TEXT)"
             cursor.execute(create_query)
             connection.commit()
 
         connection.close()
 
-
-    else:  # Create Database and URLs table
+    else:
+        # Create Database and URLs table
         connection = sqlite3.connect(DB_PATH)
         cursor = connection.cursor()
         create_query = "CREATE TABLE urls (id PRIMARY KEY, url TEXT)"
         cursor.execute(create_query)
         connection.commit()
         connection.close()
+
+
+def get_url(id: int) -> Union[str, None]:
+    """
+       Fetching the Long Url by its ID.
+       :return str or None
+    """
+    connection = sqlite3.connect(DB_PATH)
+    cursor = connection.cursor()
+    query = "SELECT url FROM urls WHERE id=(?)"
+    cursor.execute(query, (id,))
+    data = cursor.fetchone()  # Returns the row of the query result [url], None if nothing query result is nothing
+
+    if data:
+        return data[0]  # Returns the found url if data is not None
+    return None
